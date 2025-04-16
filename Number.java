b4381c4d10e297d59564e310200c527ce7002fd4
Vigneshwaralingam M <Vigneshvign1809@gmail.com>
2025-04-16 16:43:32 +0530

    number.java

---
+++ Number.java
@@ -0,0 +1,43 @@
+public class Number{
+    public static void main(String[] args){
+        int no = 7889;
+        while(no>9){
+            if(no>9){
+                no = check(no); 
+             }
+        }
+        System.out.println("Number : " + no);
+            
+    }
+
+    public static int check(int no){
+        int no1 = no % 100;
+        int no2 = no / 100;
+    
+        while(no>9){
+            if(no1>9){
+                no1 = sumOfDigits(no1);
+            }else{
+                no = no1 + no2;
+                return no;
+            }
+    
+            if(no2>9){
+                no2 = sumOfDigits(no2);
+            }else{
+                no = no1 + no2;
+                return no;
+            }
+        }
+        return no;
+    }
+
+    public static int sumOfDigits(int no){
+        int sum = 0;
+        while(no>0){
+            sum += (no%10);
+            no /= 10;
+        }
+        return sum;
+    }
+}
