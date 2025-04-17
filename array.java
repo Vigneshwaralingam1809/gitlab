57bc60b3d754c5e7afbf0009653df7f8b98f0e2d
Vigneshwaralingam M <Vigneshvign1809@gmail.com>
2025-04-17 17:45:02 +0530

    array

---
+++ array.java
@@ -0,0 +1,30 @@
+class array{
+public static void main(String []args)
+{
+int[] name = new int[5];
+name[0]=1001;
+name[1]=10002;
+name[2]=10003;
+name[3]=10004;
+name[4]=10009;
+
+
+System.out.println(name[4]+" hii "+name.length);
+first(name);
+}
+
+
+
+public static void first(int[] name)
+{
+for (int i=0; i<= name.length -1 ;i++)
+    {
+        System.out.println(name[i]);
+
+    }
+
+System.out.println(" hii ");
+
+}
+
+}
