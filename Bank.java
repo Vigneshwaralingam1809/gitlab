54b66015cea82d93882317e4596a795f2ca9b3cc
Vigneshwaralingam M <Vigneshvign1809@gmail.com>
2025-04-22 22:01:20 +0530

    22.04.2025

---
+++ Bank.java
@@ -0,0 +1,53 @@
+public class Bank{
+String name;
+int balance;
+int accountNumber;
+
+
+
+public static void main(String []args)
+{
+
+
+System.out.println("hi");
+Bank vignesh = new Bank("vignesh",2000,101);
+Bank bala = new Bank("bala",7000000,102);
+bala.deposit(10000);
+
+}
+
+public static void displayBalance()
+{
+
+
+
+
+
+}
+public static void withdraw()
+{
+
+
+
+
+
+}
+
+public static void deposit(int amount)
+{
+
+bala.balance=balance+amount;
+System.out.println(balance);
+
+
+
+}
+ Bank(String name, int initialBalance,int accno) {
+        bala.accountNumber = accno;
+        bala.accountHolderName = name;
+        bala.balance = initialBalance;
+    }
+
+
+
+}
