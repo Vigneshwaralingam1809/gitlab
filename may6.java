e2ad98594456e5f7b7ca5c737467019d7561ba3f
vigneshwaralingam <m.vigneshwaralingam@gmail.com>
2025-05-07 11:46:01 +0530

    all

---
+++ may6.java
@@ -0,0 +1,15 @@
+class May6{
+public static void main(String[]args)
+{
+//int[] arr =new arr[5];
+
+//for(int i=0;i<arr.length;i++)
+//{System..out,print(arr[i]);
+
+String[] arr={"vigneshwaralingam"};
+
+char name=arr.toCharArray();
+System.out.print(arr[4]);
+
+}
+}
